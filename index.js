d8742093355258bbfc6b920593615618af356c2d
Quinlin Gregg <44556872+TheQantas@users.noreply.github.com>
2024-05-20 20:22:49 -0600

    Update index.js
    Fixed tab select code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let links = null;
 let sections = null;
 let lastScrollInMs = null;
+let homeHeight = 400;
 
 window.onload = () => {
     links = document.querySelectorAll('nav > a');
@@ -10,6 +11,8 @@ window.onload = () => {
     }
     fadeInSections();
     updateSelectedLink();
+    sections[0].classList.add('fade-in');
+    homeHeight = document.getElementById('home-grid').clientHeight;
 }
 
 window.onscroll = () => {
@@ -27,7 +30,7 @@ function fadeInSections() {
     }
     for (const section of sections) {
         const rect = section.getBoundingClientRect();
-        if (rect.top < window.innerHeight - 50) {
+        if (rect.top < window.innerHeight - 80) {
             section.classList.add('fade-in');
         }
     }
@@ -37,17 +40,21 @@ function updateSelectedLink() {
     if (sections == null) {
         return;
     }
-    if (window.scrollY < 360) {
+    const scrollY = window.scrollY || window.pageYOffset;
+    const innerHeight = window.innerHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const atMaxScroll = scrollY + innerHeight >= scrollHeight - 100;
+    if (window.scrollY < homeHeight - 40) {
         selectNthLink(0);
+    } else if (atMaxScroll) {
+        selectNthLink(links.length-1);
     } else {
         const target = window.innerHeight / 2;
         let minDist = Infinity;
         let selIndex = 0;
-        console.log();
         for (let i = 0; i < sections.length; i++) {
             const top = sections[i].getBoundingClientRect().top;
             const dist = target - top;
-            console.log(i,'dist',dist);
             if (dist > 0 && dist < minDist) {
                 minDist = dist;
                 selIndex = i + 1;
@@ -60,6 +67,9 @@ function updateSelectedLink() {
 function selectNthLink(index) {
     if (links == null) {
         return;
+    }
+    if (index > 0) {
+        sections[index-1].classList.add('force-in');
     }
     for (let i = 0; i < links.length; i++) {
         if (i == index) {
